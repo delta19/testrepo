011b965b098c3342bfa5c4b8c74c335b0453a0ed
delta19 <delta@delta-pc>
2015-11-13 14:23:22 -0600

    branch commit 2

---
+++ src/m01.cpp
@@ -0,0 +1,5 @@
+//
+// Created by delta on 11/13/2015.
+//
+
+#include "m01.h"
---
+++ src/m01.h
@@ -0,0 +1,16 @@
+//
+// Created by delta on 11/13/2015.
+//
+
+#ifndef TESTREPO_M01_H
+#define TESTREPO_M01_H
+
+
+
+class m01 {
+
+};
+
+
+
+#endif //TESTREPO_M01_H
